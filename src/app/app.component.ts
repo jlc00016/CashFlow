c253355cee8ec7e3378d12ca9d63e3a5fd34584a
jlc00016 <jlc00016@gmail.com>
2016-08-02 12:12:07 +0200

    Creating my first component: 'movement'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { MovementComponent } from './movement'
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [MovementComponent]
 })
 export class AppComponent {
   title = 'app works!';
---
+++ src/app/movement/movement.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-movement',
+  templateUrl: 'movement.component.html',
+  styleUrls: ['movement.component.css']
+})
+export class MovementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
