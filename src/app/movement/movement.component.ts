357629e4988d5b1caf1d660132f7842c22ef7b31
jlc00016 <jlc00016@gmail.com>
2016-08-03 18:26:30 +0200

    Practical examples of 'DataBinding'

--- src/app/movement/movement.component.ts
+++ src/app/movement/movement.component.ts
@@ -8,9 +8,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovementComponent implements OnInit {
 
+  entryCategories: string[] = ['Nómina', 'Venta', 'Interés', 'Impuesto'];
+  spendingCategories: string[] = ['Hipoteca', 'Compra', 'Interés', 'Impuesto'];
+  movement: MovementModel;
+  movements: MovementModel[] = [];
+  sortDirection: number = 1;
+  incomes: number = 0;
+  expenses: number = 0;
+  balance: number = 0;
+
   constructor() { }
 
   ngOnInit() {
+    this.movement = {
+      id: new Date().toDateString(),
+      kind: "Ingreso",
+      category: "Nómina",
+      date: new Date(),
+      amount: 0
+    }
+  }
+  
+  saveMovement() {
+    if (this.movement.kind === 'Ingreso') {
+      this.incomes += this.movement.amount
+    } else {
+      this.expenses += this.movement.amount
+    }
+    this.balance = this.incomes - this.expenses
+    this.movements.push(Object.assign({}, this.movement))
+  }  
+
+  orderBy(field: string) {
+    this.sortDirection = -1 * this.sortDirection
+    this.movements.sort((a, b) => a[field] < b[field] ? this.sortDirection : -1 * this.sortDirection)
   }
 
+  stringToDate(string: string) {
+    return new Date(string)
+  }
+
+}
+
+//TODO exported to a separate file
+export class MovementModel {
+  id: string;
+  kind: string;
+  category: string;
+  date: Date;
+  amount: number;
 }
